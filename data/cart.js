77f440ef9126da003a2b62fd5e6400a1ccca7cda
Rasel <mdr889299@gmail.com>
2023-12-09 16:25:41 +0600

    remove products from cart when click delet

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,12 @@
-export const cart=[{
+export let cart=[{
   productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity:0
+  quantity:2
 },{
   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity:0
+  quantity:1
 },{
   productId:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-  quantity:0
+  quantity:3
 }];
 
 export function addToCart(productId){
@@ -26,3 +26,13 @@ export function addToCart(productId){
         };
    };
   
+  export function removeFromCart(productId){
+     const newCart=[];
+
+     cart.forEach((cartItem)=>{
+      if(cartItem.productId !== productId){
+        newCart.push(cartItem)
+      }
+     });
+     cart = newCart;
+   }
